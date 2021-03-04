d86e1d1f8132093ea9a5e3afc81292a0256f6c40
홍성은 <hongse21@gmail.com>
2021-03-04 10:49:38 +0900

    Add .gitignore and mysettings.py

--- cafe_project/settings.py
+++ cafe_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import mysettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "^u-y)^qrwpf7q%6e%u7e0d4!px@bz&99@@0ua(3y=%3_0te&53"
+SECRET_KEY = mysettings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +75,7 @@ WSGI_APPLICATION = "cafe_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
---
+++ mysettings.py
@@ -0,0 +1,8 @@
+SECRET = "^u-y)^qrwpf7q%6e%u7e0d4!px@bz&99@@0ua(3y=%3_0te&53"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
